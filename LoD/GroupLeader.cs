796238559c84c9879002043c245b19c1f4611272
孟帅洋 <mengshuaiyang@hotmail.com>
2016-07-11 23:07:57 +0800

    迪米特法则:一个对象应该对其他对象有最少的了解。

---
+++ LoD/GroupLeader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LoD
+{
+    public class GroupLeader
+    {
+        private List<Girl> listGirls;
+        //传递全班的女生进来
+        public GroupLeader(List<Girl> _listGirls)
+        {
+            this.listGirls = _listGirls;
+        }
+        //清查女生数量
+        public void countGirls()
+        {
+            Console.WriteLine("女生数量是：" + this.listGirls.Count);
+        }
+    }
+}
---
+++ LoD/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 迪米特法则
+/// 一个对象应该对其他对象有最少的了解。
+/// </summary>
+
+namespace LoD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //产生一个女生群体
+            ArrayList<Girl> listGirls = new ArrayList<Girl>();
+            //初始化女生
+            for (int i = 0; i < 20; i++)
+            {
+                listGirls.Add(new Girl());
+            }
+            Teacher teacher = new Teacher();
+            //老师发布命令
+            teacher.commond(new GroupLeader(listGirls));
+        }
+    }
+}
